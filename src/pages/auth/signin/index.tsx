80ca28b3b4db5e4c1d784eb879a52a35d6e4e8f1
Reşat Yavçin <resatyavcin@icloud.com>
2025-12-28 15:55:08 +0300

    feat: initialize project

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthLayout from "../layout";
+
+const SigninPage = () => {
+  return (
+    <AuthLayout>
+      <div>SigninPage</div>
+    </AuthLayout>
+  );
+};
+
+export default SigninPage;
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthLayout from "../layout";
+
+const SignupPage = () => {
+  return (
+    <AuthLayout>
+      <div>SignUpPage</div>
+    </AuthLayout>
+  );
+};
+
+export default SignupPage;
